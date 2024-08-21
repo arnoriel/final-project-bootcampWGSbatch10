d7e4ec04c4c856d457cbd33627b3d8af050b4ad4
arnoriel <azrilluthfimulyadi@gmail.com>
2024-08-21 14:12:39 +0700

    login

--- backend/index.js
+++ backend/index.js
@@ -51,43 +51,44 @@ app.get('/login', (req, res) => {
 
 // Route untuk memproses login
 app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Cek apakah user ada
-    const userQuery = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-    const user = userQuery.rows[0];
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+    const { email, password } = req.body;
+  
+    try {
+      // Cek apakah user ada
+      const userQuery = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+      const user = userQuery.rows[0];
+  
+      if (!user) {
+        return res.render('login', { title: 'Login', error: 'Email/Password salah' });
+      }
+  
+      // Bandingkan password
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.render('login', { title: 'Login', error: 'Email/Password salah' });
+      }
+  
+      // Buat JWT Token
+      const token = jwt.sign({ user_id: user.id, role: user.role }, 'AsdgaSdagstdw-1h012hdsak', { expiresIn: '1h' });
+  
+      // Simpan token dan data user di session
+      req.session.token = token;
+      req.session.user = user;
+  
+      // Redirect berdasarkan role
+      if (user.role === 'superadmin') {
+        return res.redirect('/superadmin');
+      } else if (user.role === 'admin') {
+        return res.redirect('/admin');
+      } else if (user.role === 'user') {
+        return res.redirect('/user');
+      }
+  
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-
-    // Bandingkan password
-    const validPassword = bcrypt.compareSync(password, user.password);
-    if (!validPassword) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    // Buat JWT Token
-    const token = jwt.sign({ user_id: user.id, role: user.role }, 'AsdgaSdagstdw-1h012hdsak', { expiresIn: '1h' });
-
-    // Simpan token dan data user di session
-    req.session.token = token;
-    req.session.user = user;
-
-    // Redirect berdasarkan role
-    if (user.role === 'superadmin') {
-      return res.redirect('/superadmin');
-    } else if (user.role === 'admin') {
-      return res.redirect('/admin');
-    } else if (user.role === 'user') {
-      return res.redirect('/user');
-    }
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+  });
+  
 
 // Route untuk dashboard superadmin
 app.get('/superadmin', (req, res) => {
@@ -119,7 +120,7 @@ app.get('/logout', (req, res) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
-    res.redirect('/login');
+    res.redirect('/');
   });
 });
 
